6451390ac9cc1da1c28b3bb044afe95acdfabb57
Alan Wei <alan.wei@shopback.com>
2017-12-12 18:54:29 +0800

    add initilizeParameters

--- src/model/initializeParameters.ts
+++ src/model/initializeParameters.ts
@@ -4,9 +4,9 @@ function initializeParameters(layerDims: Array<number>) {
   const parameters: any = {};
   const l = layerDims.length;
   
-  for (let i = 1; i <= l; i++) {
-    parameters[`W${i}`] = math.randn(i, i+1);
-    parameters[`b${i}`] = math.zeros(i, 1);
+  for (let i = 1; i < l; i++) {
+    parameters[`W${i}`] = math.randn(layerDims[i], layerDims[i-1]);
+    parameters[`b${i}`] = math.zeros(layerDims[i], 1);
   }
 
   return parameters;
