bc2011ef14d582920e01c4cfc253b12154b82ff4
Alan Wei <alan.wei@shopback.com>
2017-12-13 11:48:45 +0800

    add tests for initializeParameters

--- src/math/randn.ts
+++ src/math/randn.ts
@@ -1,11 +1,12 @@
 const gaussian = require('gaussian');
 
-function randn(dim1: number = 1, dim2: number = 1) {
+function randn(dim1: number = 1, dim2: number = 1,
+  mean: number = 0, variance: number = 1) {
   const ro = [];
   for (let i = 0; i < dim2; i++) {
     const row = [];
     for (let j = 0; j < dim1; j++) {
-      const distribution = gaussian(0, 1);
+      const distribution = gaussian(mean, variance);
       const sample = distribution.ppf(Math.random());
       row.push(sample);
     }
---
+++ src/model/initializeParameters.spec.ts
@@ -0,0 +1,22 @@
+import initializeParameters from './initializeParameters';
+
+test('2 layers neural network', () => {
+  const ro = initializeParameters([2, 1]);
+  expect(ro['W1'].length).toEqual(2);
+  expect(ro['W1'][0].length).toEqual(1);
+  expect(ro['b1']).toEqual([[0]]);
+});
+
+test('3 layers neural network', () => {
+  const ro = initializeParameters([3, 2, 1]);
+  expect(ro['W1'].length).toEqual(3);
+  expect(ro['W1'][0].length).toEqual(2);
+  expect(ro['b1']).toEqual([[0, 0]]);
+});
+
+test('5 layers neural network', () => {
+  const ro = initializeParameters([5, 4, 3, 2, 1]);
+  expect(ro['W1'].length).toEqual(5);
+  expect(ro['W1'][0].length).toEqual(4);
+  expect(ro['b1']).toEqual([[0, 0, 0, 0]]);
+});
