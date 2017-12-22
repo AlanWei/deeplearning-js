b07bb8a4a8dc1446f6ecab30d8a20145dbc57f32
Alan Wei <alan.wei@shopback.com>
2017-12-22 18:23:48 +0800

    add test

--- src/data/read.ts
+++ src/data/read.ts
@@ -4,8 +4,6 @@ import { map, slice } from 'lodash';
 import Model from '../model';
 import math from '../math';
 
-// const start = Date.now();
-
 function read(targetNum: number) {
   const Y: any = [];
   const X: any = [];
@@ -33,25 +31,22 @@ function read(targetNum: number) {
         size: x[0].length,
         activationFunc: '',
       }, {
-        size: 4,
-        activationFunc: 'relu',
+        size: 5,
+        activationFunc: 'linear',
       }, {
         size: 1,
         activationFunc: 'sigmoid',
       }], 0, 1, 0.01);
 
-      for (let i = 1; i <= 10000; i++) {
+      for (let i = 1; i <= 1000; i++) {
         map(x, (example: Array<Array<number>>, idx) => {
           const forward = Model.forwardPropagation(example, parameters);
           const grads = Model.backPropagation(
-            math.logProbBackward,
+            math.subtract,
             Y[idx],
             forward,
           );
-          // console.log(parameters['W1']);
-          // console.log(grads['dW1'], 'dW1');
           parameters = Model.updateParameters(parameters, grads, 0.0075);
-          // console.log(parameters['W1']);
         });
 
         if (i % 100 === 0) {
@@ -60,7 +55,7 @@ function read(targetNum: number) {
           map(x, (example: Array<Array<number>>, idx) => {
             const ro = Model.forwardPropagation(example, parameters);
             const forward = ro.AL;
-            const cost = Model.computeCost(forward, Y[idx], math.logProb);
+            const cost = Model.computeCost(forward, Y[idx], math.absDiff);
             predict.push(forward);
             costs.push(cost);
           });
@@ -76,7 +71,6 @@ function read(targetNum: number) {
               ))
             ))
           ));
-          // console.log(predict);
           let correct = 0;
           map(predict, (subArr: Array<Array<number>>, idx) => (
             map(subArr, (arr, i) => (
@@ -89,10 +83,69 @@ function read(targetNum: number) {
           ));
           const m = Y.length;
           console.log(`Accuracy: ${correct / m * 100}%`);
+          console.log(`Correct count: ${correct}`);
         }
       }
 
-      // console.log('time:', (Date.now() - start) / 1000);
+      const X2: any = [];
+      const Y2: any = [];
+      let count = 0;
+      fs.createReadStream("./mnist_test.csv")
+        .pipe(csv())
+        .on("data", (data: any) => {
+          X2[count] = [];
+          Y2[count] = [];
+          const output = parseInt(data[0], 10);
+          const input = slice(data, 1);
+          if (output === targetNum) {
+            Y2[count].push([1]);
+          } else {
+            Y2[count].push([0]);
+          }
+          map(input, (num: string, idx) => {
+            X2[count][idx] = [parseInt(num, 10)];
+          });
+          count++;
+        })
+        .on("end", () => {
+          const x = X2;
+          let predict: any = [];
+          const costs: any = [];
+          map(x, (example: Array<Array<number>>, idx) => {
+            const ro = Model.forwardPropagation(example, parameters);
+            const forward = ro.AL;
+            const cost = Model.computeCost(forward, Y2[idx], math.absDiff);
+            predict.push(forward);
+            costs.push(cost);
+          });
+          let costSum = 0;
+          map(costs, (cost: number) => (
+            costSum += cost
+          ));
+          console.log(`Cost is ${costSum / costs.length}`);
+          predict = map(predict, (subArr: Array<Array<number>>) => (
+            map(subArr, (arr) => (
+              map(arr, (num) => (
+                num > 0.5 ? 1 : 0
+              ))
+            ))
+          ));
+          let correct = 0;
+          map(predict, (subArr: Array<Array<number>>, idx) => (
+            map(subArr, (arr, i) => (
+              map(arr, (num, j) => {
+                if (num === Y2[idx][i][j]) {
+                  correct++;
+                } else {
+                  console.log(idx);
+                }
+              })
+            ))
+          ));
+          const m = Y2.length;
+          console.log(`Test Accuracy: ${correct / m * 100}%`);
+          console.log(`Test Correct count: ${correct}`);
+        });
     });
 }
 
