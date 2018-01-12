a988cdd3e1baad7723623e45aafd5888c8fb9d4e
Alan Wei <alan.wei@shopback.com>
2018-01-12 09:59:18 +0800

    simplify backward function

--- src/math/sigmoidBackward.ts
+++ src/math/sigmoidBackward.ts
@@ -1,18 +1,17 @@
 import { map } from 'lodash';
+import sigmoid from './sigmoid';
 import Array2D from '../data/Array2D';
 
 function sigmoidBackward(
   dA: Array2D,
   cache: Array2D,
 ) {
-  const dAValues = dA.values;
-  const cacheValues = cache.values;
-  const zValues = map(cacheValues, (num, idx) => {
-    const s = 1 / (1 + Math.exp(-num));
-    return dAValues[idx] * s * (1 - s);
-  });
+  const sigmoidCache = sigmoid(cache);
+  const values = map(sigmoidCache.A.values, (num, idx) => (
+    dA.values[idx] * num * (1 - num)
+  ));
 
-  return new Array2D(cache.shape, zValues);
+  return new Array2D(cache.shape, values);
 }
 
 export default sigmoidBackward;
--- src/math/softmaxBackward.ts
+++ src/math/softmaxBackward.ts
@@ -1,4 +1,5 @@
 import { map, sum } from 'lodash';
+import softmax from './softmax';
 import Array2D from '../data/Array2D';
 import convertArray2DToArray1D from '../utils/convertArray2DToArray1D';
 
@@ -6,20 +7,12 @@ function softmaxBackward(
   dA: Array2D,
   cache: Array2D,
 ) {
-  const values = map(cache.values, (num) => (Math.exp(num)));
-  const cacheT = new Array2D(cache.shape, values).transpose();
-  const dAT = dA.transpose();
-  const derivativeMatrix = map(cacheT.matrix, (subArray, i) => (
-    map(subArray, (num, j) => {
-      const softmaxNum = num / sum(subArray);
-      return dAT.matrix[i][j] * softmaxNum * (1 - softmaxNum);
-    }
-  )));
+  const softmaxCache = softmax(cache);
+  const values = map(softmaxCache.A.values, (num, idx) => (
+    dA.values[idx] * num * (1 - num)
+  ));
 
-  return new Array2D(
-    cacheT.shape,
-    convertArray2DToArray1D(cacheT.shape, derivativeMatrix),
-  ).transpose();
+  return new Array2D(cache.shape, values);
 }
 
 export default softmaxBackward;
