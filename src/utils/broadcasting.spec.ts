0a0e2b07ee92c86684c3b8d0486368d5a96f9f59
HungMingWu <u9089000@gmail.com>
2018-01-16 07:19:54 +0800

    Fix vertical matrix expansion implementation

--- src/utils/broadcasting.spec.ts
+++ src/utils/broadcasting.spec.ts
@@ -8,6 +8,13 @@ test('col broadcasting', () => {
   expect(ro).toEqual(new Array2D([1, 3], [1, 1, 1]));
 });
 
+test('col broadcasting 2', () => {
+  const left = new Array2D([2, 3], [1, 2, 3, 4, 5, 6]);
+  const right = new Array2D([2, 1], [1, 2]);
+  const ro = broadcasting(left, right).right;
+  expect(ro).toEqual(new Array2D([2, 3], [1, 1, 1, 2, 2, 2]));
+});
+
 test('row broadcasting', () => {
   const left = new Array2D([3, 1], [1, 2, 3]);
   const right = new Array2D([1, 1], [1]);
