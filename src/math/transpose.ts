dd8021bd21c8bbef25fe3224bf41a12e6bc21393
Alan Wei <alan.wei@shopback.com>
2018-03-01 18:22:26 +0800

    gpu transpose

--- src/math/transpose.ts
+++ src/math/transpose.ts
@@ -1,22 +1,28 @@
-import { map } from 'lodash';
 import { Array2D } from '../data/';
 import { convertArray2DToArray1D } from '../utils';
+
+const GPU = require('gpu.js');
+const gpu = new GPU();
+
+const gpuTranspose = (a: Array2D) => {
+  const transpose = gpu.createKernel(function(this: any, a: any) {
+    return a[this.thread.x][this.thread.y];
+  }).setOutput([a.shape[0], a.shape[1]]);
+
+  return transpose(a.matrix);
+};
 
 function transpose(
   matrix: Array2D,
 ) {
-  const newMatrix: Array<Array<number>> = map(matrix.matrix[0], (col, i) => (
-    map(matrix.matrix, (row) => (
-      row[i]
-    ))
-  ));
+  const newMatrix = gpuTranspose(matrix);
 
-  const row = matrix.shape[0];
-  const col = matrix.shape[1];
+  const values = convertArray2DToArray1D(
+    [matrix.shape[1], matrix.shape[0]],
+    newMatrix,
+  );
 
-  const values = convertArray2DToArray1D([col, row], newMatrix);
-
-  return new Array2D([col, row], values);
+  return new Array2D([matrix.shape[1], matrix.shape[0]], values);
 }
 
 export default transpose;
