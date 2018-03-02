df91876edc7ec00fb284ddf2a3690e13139aaf13
Alan Wei <alan.wei@shopback.com>
2018-03-02 12:15:47 +0800

    revert jest version & add gpu randn

--- src/math/randn.ts
+++ src/math/randn.ts
@@ -1,24 +1,20 @@
 const gaussian = require('gaussian');
-import { Array2D } from '../data/';
+const GPU = require('gpu.js');
+const gpu = new GPU();
 
-function randn(
+const randn = (
   shape: [number, number],
   mean: number = 0,
   variance: number = 1,
   scale: number = 1,
-): Array2D {
-  const row: number = shape[0];
-  const col: number = shape[1];
-  const values: Array<number> = [];
-  for (let i = 0; i < row; i++) {
-    for (let j = 0; j < col; j++) {
-      const distribution = gaussian(mean, variance);
-      const sample: number = distribution.pdf(Math.random()) * scale;
-      values.push(sample);
-    }
-  }
-
-  return new Array2D(shape, values);
-}
+): Array<Array<number>> => {
+  const distribution = gaussian(mean, variance);
+  return gpu.createKernel(function(this: any) {
+    return this.constants.distribution.pdf(Math.random())* this.constants.scale;
+  }, {
+    constants: { distribution, scale },
+    output: [shape[1], shape[0]],
+  })();
+};
 
 export default randn;
