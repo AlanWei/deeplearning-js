0912a59471acaf216a82d795b28295787bb2c6cc
Alan Wei <alan.wei@shopback.com>
2018-03-03 23:06:50 +0800

    tweak test

--- src/activationFunction/reluBackward.spec.ts
+++ src/activationFunction/reluBackward.spec.ts
@@ -1,8 +1,8 @@
 import reluBackward from './reluBackward';
 
 test('valid reluBackward', () => {
-  const dA = [[2, -1]];
-  const cache = [[1, -1]];
+  const dA = [[2, -1, 3]];
+  const cache = [[1, -1, 1]];
   const ro = reluBackward(dA, cache);
-  expect(ro).toEqual([[1, 0]]);
+  expect(ro).toEqual([[2, 0, 3]]);
 });
